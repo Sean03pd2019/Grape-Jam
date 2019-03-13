69a1910c9fa7d6efb74ce7d47fae01fda0567eeb
Lilian Chu <lilian.emchu@gmail.com>
2019-03-13 15:43:18 -0700

    Puzzle Parts
    -Added "Button" prefab
    -Added "ButtonBehavior" and "PlayerWeight" (C# scripts) to Scripts folder
    -All buttons will now have a necessary weight to press them. Player's default weight is 5, and each block will have a TBD weight

---
+++ Grape Jam/Assets/Scripts/ButtonBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehavior : MonoBehaviour
+{
+
+    public Sprite Pressed;
+    public int playerWeight;
+    public int necessaryWeight;
+
+    [SerializeField] private PolygonCollider2D[] colliders;
+    private int currentColliderIndex = 0;
+
+    public void Start()
+    {
+        colliders[currentColliderIndex].enabled = true;
+    }
+
+    public void SetColliderForSprite(int spriteNum)
+    {
+        colliders[currentColliderIndex].enabled = false;
+        currentColliderIndex = spriteNum;
+        colliders[spriteNum].enabled = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject player = GameObject.Find("Player");
+        PlayerWeight script = player.GetComponent<PlayerWeight>();
+        playerWeight = script.weight;
+
+        if (playerWeight >= necessaryWeight)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = Pressed;
+            SetColliderForSprite(1);
+        }
+    }
+}
