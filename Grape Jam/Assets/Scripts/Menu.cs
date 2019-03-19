5997e46f88ea5da9a298c3082549a84d7655192e
Sean Jan <sean03pd2019@saschina.org>
2019-03-19 12:03:53 +0800

    .

---
+++ Grape Jam/Assets/Scripts/Menu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Update is called once per frame
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("World1");
+    }
+}
